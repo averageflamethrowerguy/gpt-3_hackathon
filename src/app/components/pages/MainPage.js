8c27fff56642e84129becc19c7a909cf7b245f38
averageflamethrowerguy <elliotpotter496@gmail.com>
2021-05-29 15:49:02 -0400

    rename thing

--- src/app/components/pages/MainPage.js
+++ src/app/components/pages/MainPage.js
@@ -6,7 +6,7 @@ import logo from '../../../assets/img/logo.png'
 import './MainPage.css'
 
 
-import regexCache from '../gtp3-utility-caches/regex-cache'
+import regexCache from '../gpt3-utility-caches/regex-cache'
 // END IMPORT SECTION
 
 
